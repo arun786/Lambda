ab2ab62a5f6fb1bdbfcd7ad4c1f9a9f1155a7601
Adwiti789 <adwiti1975@gmail.com>
2018-04-12 23:31:11 -0700

    Use of Predicate

---
+++ src/main/java/com/model/useOfPredicate.java
@@ -0,0 +1,39 @@
+package com.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+/**
+ * Created by Adwiti on 4/12/2018.
+ */
+public class useOfPredicate {
+    public static void main(String[] args) {
+        Employee e1 = new Employee("arun", 31);
+        Employee e2 = new Employee("sachin", 45);
+        Employee e3 = new Employee("dhoni", 36);
+        Employee e4 = new Employee("rahul", 4);
+        Employee e5 = new Employee("adwiti", 3);
+
+        List<Employee> employees = new ArrayList<>();
+        employees.add(e1);
+        employees.add(e2);
+        employees.add(e3);
+        employees.add(e4);
+        employees.add(e5);
+
+        printEmployeeByAge(employees, "Employee greater than 30", employee -> employee.getAge() > 30);
+        printEmployeeByAge(employees, "Employee less than 30", employee -> employee.getAge() < 30);
+
+    }
+
+    public static void printEmployeeByAge(List<Employee> employees, String textCondition, Predicate<Employee> ageCondition) {
+        System.out.println(textCondition);
+        System.out.println("-------------------");
+        for (Employee emp : employees) {
+            if (ageCondition.test(emp)) {
+                System.out.println(emp.getName() + ": " + emp.getAge());
+            }
+        }
+    }
+}
